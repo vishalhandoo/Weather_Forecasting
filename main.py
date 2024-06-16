3e3d856121b01937cffb365abe5ca4aec48e4b20
vishalhandoo <vishalhandoo067@gmail.com>
2024-06-16 23:25:07 +0530

    Collected the needed data such as Temperature and Condition of weather In streamlit

--- main.py
+++ main.py
@@ -12,10 +12,20 @@ options = st.selectbox("Select data to view",
 
 st.subheader(f"{options} for the next {days} days in {place}")
 
+if place:
+    try:
+        filtered_data = get_data(place, days)
 
-data = get_data(place, days, options)
-
-d, t = get_data(days)
-
-figure = px.line(x=d, y=t, labels={"x": "Date", "y": "Temperature(C)"})
-st.plotly_chart(figure)
+        if options == "Temperature":
+            temperatures = [dict["main"]["temp"]-273 for dict in filtered_data]
+            dates = [dict["dt_txt"] for dict in filtered_data]
+            figure = px.line(x=dates, y=temperatures, labels={"x": "Date", "y": "Temperature(C)"})
+            st.plotly_chart(figure)
+        if options == "Sky":
+            images = {"Clear": "images/clear.png", "Clouds": "images/cloud.png",
+                      "Rain": "images/rain.png", "Snow": "images/snow.png"}
+            sky_condition = [dict["weather"][0]["main"] for dict in filtered_data]
+            image_path = [images[condition] for condition in sky_condition]
+            st.image(image_path,width=115)
+    except KeyError:
+        st.info("That Place does not exist")
